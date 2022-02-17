c151a654349fa08ec9a59523e9451b3090ec970e
Sertorio Morales <sertomorales@gmail.com>
2022-02-17 18:58:29 +0100

    exe 4

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@
 import React from 'react';
 import Escena from './components/escena/Escena';
 import Messages from './Messages';
+import {useState} from 'react';
 
 function App() {
 
-    const escenas = Messages.map( (message, id) => <Escena textEscena={message} key={id} />);
+    const [frase, setFrase] = useState(1); 
+
+    const prev = () => { (frase === 1) ? setFrase(4) : setFrase(frase - 1) ;}
+    const next = () => { (frase === 4) ? setFrase(1) : setFrase(frase + 1) ;}
+
+
+    const escenas = Messages.map( 
+      ( message, frase, id) => 
+        <Escena textEscena={message} key={id} fraseNum={frase}/>
+      );
 
     return ( 
       <div className = "App">
 
+        <button onClick={prev}>Anterior</button>
+
+        <button onClick={next}>Siguiente</button>
+
         {escenas}
+
+        <p>contador : {frase}</p>
 
       </div>
     );
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -2,4 +2,4 @@
 import React from 'react';
 import { EscenaStyles } from './Escena.styles';
 
-export default (props) => <EscenaStyles><p>{props.textEscena}</p></EscenaStyles>
+export default (props) => <EscenaStyles><p>{props.textEscena} - {props.fraseNum}</p></EscenaStyles>
